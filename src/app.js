0315c4061f7691349dbbc97be5f68abd44ffab18
RobinOShiaa <oshea.robinho@gmail.com>
2022-05-18 16:04:21 +0100

    init commit

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ app.get('/weather', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Robin O\'Shea',
         errorMessage: 'Page not found.'
     })
 })
